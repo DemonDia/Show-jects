a140feb1773f0df59441fa15f0e14f3e35bb94ea
Setsuna Yukiii <siang-meng@live.com>
2023-01-24 19:25:57 +0800

    adding search as you type

--- src/Components/Projects/ProjectListContainer.js
+++ src/Components/Projects/ProjectListContainer.js
@@ -9,6 +9,7 @@ function ProjectListContainer({
     handleDelete,
 }) {
     const [search, setSearch] = useState("");
+    const [sortBy, setSortBy] = useState(0);
     return (
         <>
             {/* search as you type & filter*/}
@@ -22,6 +23,9 @@ function ProjectListContainer({
                     <Grid item xs={12} sm={8}>
                         <TextField
                             value={search}
+                            onChange={(e) => {
+                                setSearch(e.target.value);
+                            }}
                             placeholder={"Search ..."}
                             variant={"outlined"}
                             sx={{ width: "100%" }}
@@ -39,18 +43,40 @@ function ProjectListContainer({
                 <Grid container>
                     {projects != null ? (
                         <>
-                            {projects.map((project, index) => {
-                                return (
-                                    <ProjectContainer
-                                        key={index}
-                                        project={project}
-                                        isOwner={isOwner}
-                                        userId={userId}
-                                        handleLike={handleLike}
-                                        handleDelete={handleDelete}
-                                    />
-                                );
-                            })}
+                            {projects
+                                .filter((project) => {
+                                    return (
+                                        project.projectName
+                                            .toLowerCase()
+                                            .includes(search.toLowerCase()) ||
+                                        project.projectDescription
+                                            .toLowerCase()
+                                            .includes(search.toLowerCase())
+                                    );
+                                })
+                                // .sort((a, b) =>
+                                //     sortBy == 1
+                                //         ? a.projectName > b.projectName
+                                //             ? 1
+                                //             : -1
+                                //         : sortBy == 2
+                                //         ? b.projectName > a.projectName
+                                //             ? 1
+                                //             : -1
+                                //         : -1
+                                // )
+                                .map((project, index) => {
+                                    return (
+                                        <ProjectContainer
+                                            key={index}
+                                            project={project}
+                                            isOwner={isOwner}
+                                            userId={userId}
+                                            handleLike={handleLike}
+                                            handleDelete={handleDelete}
+                                        />
+                                    );
+                                })}
                         </>
                     ) : (
                         <></>
