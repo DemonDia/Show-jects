36bafdd2c5b064dbfb6da75a32aff133ee7a3383
Setsuna Yukiii <siang-meng@live.com>
2023-02-11 18:02:37 +0800

    .

--- src/Components/Projects/ProjectListContainer.js
+++ src/Components/Projects/ProjectListContainer.js
@@ -25,64 +25,55 @@ function ProjectListContainer({
     console.log("pathname", pathname);
     return (
         <>
-            {/* search as you type & filter*/}
-            <Card
-                sx={{
-                    margin: "10px auto",
-                    padding: "10px",
-                }}
-            >
-                <>
-                    {!pathname.includes("/user/profile/") ? (
-                        <>
-                            <Grid container spacing={2}>
-                                <Grid item xs={12} sm={8}>
-                                    <TextField
-                                        value={search}
-                                        onChange={(e) => {
-                                            setSearch(e.target.value);
-                                        }}
-                                        placeholder={"Search ..."}
-                                        variant={"outlined"}
-                                        sx={{
-                                            width: "100%",
-                                            margin: "10px auto",
-                                        }}
-                                    />
-                                </Grid>
-                                <Grid item xs={12} sm={4}>
-                                    <Select
-                                        value={sortBy}
-                                        onChange={(e) => {
-                                            setSortBy(e.target.value);
-                                        }}
-                                        sx={{
-                                            width: "100%",
-                                            margin: "10px auto",
-                                            textAlign: "left",
-                                        }}
-                                    >
-                                        {sortingOrder.map(
-                                            (sortingOption, index) => {
-                                                return (
-                                                    <MenuItem
-                                                        value={index}
-                                                        key={index}
-                                                    >
-                                                        {sortingOption}
-                                                    </MenuItem>
-                                                );
-                                            }
-                                        )}
-                                    </Select>
-                                </Grid>
-                            </Grid>
-                        </>
-                    ) : (
-                        <></>
-                    )}
-                </>
-            </Card>
+            {!pathname.includes("/user/profile/") ? (
+                <Card
+                    sx={{
+                        margin: "10px auto",
+                        padding: "10px",
+                    }}
+                >
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={8}>
+                            <TextField
+                                value={search}
+                                onChange={(e) => {
+                                    setSearch(e.target.value);
+                                }}
+                                placeholder={"Search ..."}
+                                variant={"outlined"}
+                                sx={{
+                                    width: "100%",
+                                    margin: "10px auto",
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Select
+                                value={sortBy}
+                                onChange={(e) => {
+                                    setSortBy(e.target.value);
+                                }}
+                                sx={{
+                                    width: "100%",
+                                    margin: "10px auto",
+                                    textAlign: "left",
+                                }}
+                            >
+                                {sortingOrder.map((sortingOption, index) => {
+                                    return (
+                                        <MenuItem value={index} key={index}>
+                                            {sortingOption}
+                                        </MenuItem>
+                                    );
+                                })}
+                            </Select>
+                        </Grid>
+                    </Grid>
+                </Card>
+            ) : (
+                <></>
+            )}
+
             {/*  */}
             <Box
                 sx={{
